89374f857fcc471a85fd3d993ed8df1d0c3e6b6c
achmad <achmad.safruddin@majoo.id>
2022-05-15 01:29:09 +0700

    initial commit

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/achmad/Development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/achmad/Development/flutter-project/attendance_app"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=/Users/achmad/Development/flutter-project/attendance_app/lib/main_development.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=/Users/achmad/Development/flutter-project/attendance_app/.dart_tool/package_config.json"
